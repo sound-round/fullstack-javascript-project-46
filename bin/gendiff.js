d1df6c93b7fcfa97b336b11d7b03bb190afc590d
sound-round <yudaev1@gmail.com>
2022-11-13 13:35:35 +0300

    Add args and format to cli-utility

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
