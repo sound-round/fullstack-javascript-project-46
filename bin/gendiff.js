fe628aa8cff1f20c1bc6f971bf3eca9ea20c37b6
sound-round <yudaev1@gmail.com>
2022-11-13 13:24:50 +0300

    Add cli-utility

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number');
+
+program.parse();
+
+export default program;
