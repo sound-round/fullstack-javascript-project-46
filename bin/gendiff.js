4c4f975e26066a913d0b787675079e4c85c665ba
sound-round <yudaev1@gmail.com>
2022-11-13 17:50:08 +0300

    Fix linter errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ program
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    genDiff(filepath1, filepath2)
+    genDiff(filepath1, filepath2);
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import * as fs from 'node:fs';
 import path from 'node:path';
-import _ from "lodash";
+import _ from 'lodash';
 
 const indent = ' ';
+
+const getKeys = (firstData, secondData) => {
+  const keys1 = Object.keys(firstData);
+  const keys2 = Object.keys(secondData);
+  const newKeys = _.difference(keys2, keys1);
+  const removedKeys = _.difference(keys1, keys2);
+  const commonKeys = _.intersection(keys1, keys2);
+  return { newKeys, removedKeys, commonKeys };
+};
+
+const makeDiff = (parts) => {
+  const strings = parts.reduce((acc, part) => {
+    acc.push(`${indent}${part.sign} ${part.key}: ${part.value}`);
+    return acc;
+  }, []);
+  const diff = `{\n${strings.join('\n')}\n}`;
+  return diff;
+};
 
 const genDiff = (filepath1, filepath2) => {
   try {
@@ -12,56 +30,39 @@ const genDiff = (filepath1, filepath2) => {
     const config1 = JSON.parse(json1);
     const config2 = JSON.parse(json2);
 
-    const parts = []
-    
-    const {newKeys, removedKeys, commonKeys} = getKeys(config1, config2);
+    const parts = [];
 
-    newKeys.map((key) => parts.push({
-      sign: "+",key: key, value: config2[key]
-    }))
-    removedKeys.map((key) => parts.push({
-      sign: "-",key: key, value: config1[key]
-    }))
-    commonKeys.map((key) => {
+    const { newKeys, removedKeys, commonKeys } = getKeys(config1, config2);
+
+    newKeys.forEach((key) => parts.push({
+      sign: '+', key, value: config2[key],
+    }));
+    removedKeys.forEach((key) => parts.push({
+      sign: '-', key, value: config1[key],
+    }));
+    commonKeys.forEach((key) => {
       if (config1[key] === config2[key]) {
         parts.push({
-          sign: " ",key: key, value: config1[key]
-        })
-        return
+          sign: ' ', key, value: config1[key],
+        });
+        return;
       }
       parts.push({
-        sign: "-",key: key, value: config1[key]
-      })
+        sign: '-', key, value: config1[key],
+      });
       parts.push({
-        sign: "+",key: key, value: config2[key]
-      })
-    })
+        sign: '+', key, value: config2[key],
+      });
+    });
 
-    const sorted_parts = _.sortBy(parts, ['key', '-sign'])
-    const diff = makeDiff(sorted_parts);
-    console.log(diff)
-    return diff
+    const sortedParts = _.sortBy(parts, ['key', '-sign']);
+    const diff = makeDiff(sortedParts);
+    console.log(diff);
+    return diff;
   } catch (err) {
     console.error(err);
   }
+  return null;
 };
-
-const getKeys = (firstData, secondData) => {
-  const keys1 = Object.keys(firstData);
-  const keys2 = Object.keys(secondData);
-  const newKeys = _.difference(keys2, keys1);
-  const removedKeys = _.difference(keys1, keys2);
-  const commonKeys = _.intersection(keys1, keys2);
-  return {newKeys, removedKeys, commonKeys};
-}
-
-const makeDiff = (parts) => {
-  const strings = parts.reduce((acc, part) => {
-    acc.push(`${indent}${part.sign} ${part.key}: ${part.value}`)
-    return acc
-  }, [])
-  const diff = `{\n${strings.join('\n')}\n}`
-  return diff
-}
 
 export default genDiff;
