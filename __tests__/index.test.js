9bde937bfc1a50547205021147f10882dbb26f96
sound-round <yudaev1@gmail.com>
2022-12-05 20:41:33 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,17 @@ import { getFixturePath, readFile } from '../utils/utils.js';
 import genDiff from '../src/index.js';
 
 describe('test gendiff', () => {
-  it('gendiff works', () => {
+  it('gendiff works with json', () => {
     const firstFilePath = getFixturePath('file1.json');
     const secondFilePath = getFixturePath('file2.json');
+    const expected = readFile('flat_result.txt').trim();
+    const result = genDiff(firstFilePath, secondFilePath);
+    expect(result).toBe(expected);
+  });
+
+  it('gendiff works with yaml', () => {
+    const firstFilePath = getFixturePath('file1.yaml');
+    const secondFilePath = getFixturePath('file2.yaml');
     const expected = readFile('flat_result.txt').trim();
     const result = genDiff(firstFilePath, secondFilePath);
     expect(result).toBe(expected);
