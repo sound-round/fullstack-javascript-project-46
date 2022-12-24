86fcb7ab92860be12a7eedc25e22688966aabdc5
sound-round <yudaev1@gmail.com>
2022-12-24 13:41:10 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,7 @@ describe('test gendiff', () => {
   it('gendiff works with json', () => {
     const firstFilePath = getFixturePath('file1.json');
     const secondFilePath = getFixturePath('file2.json');
-    const expected = readFile('flat_result.txt').trim();
+    const expected = readFile('result.stylish').trim();
     const result = genDiff(firstFilePath, secondFilePath);
     expect(result).toBe(expected);
   });
@@ -13,7 +13,7 @@ describe('test gendiff', () => {
   it('gendiff works with yaml', () => {
     const firstFilePath = getFixturePath('file1.yaml');
     const secondFilePath = getFixturePath('file2.yaml');
-    const expected = readFile('flat_result.txt').trim();
+    const expected = readFile('result.stylish').trim();
     const result = genDiff(firstFilePath, secondFilePath);
     expect(result).toBe(expected);
   });
