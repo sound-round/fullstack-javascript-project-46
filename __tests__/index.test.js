cbbd83c033377421424cd07ee8c053f54469f5d6
sound-round <yudaev1@gmail.com>
2022-12-25 13:39:47 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { getFixturePath, readFile } from '../utils/utils.js';
 import genDiff from '../src/index.js';
 
-describe('test gendiff', () => {
+describe('test gendiff stylish format', () => {
   it('gendiff works with json', () => {
     const firstFilePath = getFixturePath('file1.json');
     const secondFilePath = getFixturePath('file2.json');
@@ -15,6 +15,24 @@ describe('test gendiff', () => {
     const secondFilePath = getFixturePath('file2.yaml');
     const expected = readFile('result.stylish').trim();
     const result = genDiff(firstFilePath, secondFilePath);
+    expect(result).toBe(expected);
+  });
+});
+
+describe('test gendiff plain format', () => {
+  it('gendiff works with json', () => {
+    const firstFilePath = getFixturePath('file1.json');
+    const secondFilePath = getFixturePath('file2.json');
+    const expected = readFile('result.plain').trim();
+    const result = genDiff(firstFilePath, secondFilePath, "plain");
+    expect(result).toBe(expected);
+  });
+
+  it('gendiff works with yaml', () => {
+    const firstFilePath = getFixturePath('file1.yaml');
+    const secondFilePath = getFixturePath('file2.yaml');
+    const expected = readFile('result.plain').trim();
+    const result = genDiff(firstFilePath, secondFilePath, "plain");
     expect(result).toBe(expected);
   });
 });
